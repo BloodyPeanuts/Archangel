84191a7080ad310a5bee810481282d1274a4c043
Hesamoddin Aran <lehesamoddinaran@gmail.com>
2022-01-26 13:36:44 +0330

    Initial Commit

---
+++ Archangel/ArchangelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchangelApp.swift
+//  Archangel
+//
+//  Created by Hesamoddin Aran  on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ArchangelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
